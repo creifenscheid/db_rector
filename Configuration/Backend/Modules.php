7ea53290c9a83eb5aa01af76ac9b840b24a839da
creifenscheid <dev@christian-reifenscheid.de>
2023-05-31 10:36:13 +0200

    [TASK] Backend module
    Move module to admin tools

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -9,14 +9,14 @@ $moduleIdentifier = GeneralUtility::underscoredToUpperCamelCase($extKey);
 
 return [
     'web_' . $moduleIdentifier => [
-        'parent' => 'web',
-        'position' => ['after' => '*'],
+        'parent' => 'tools',
+        'position' => ['after' => 'tools_toolsupgrade'],
         'access' => 'admin',
         'iconIdentifier' => 'db-rector-extension',
         'labels' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_mod.xlf',
         'extensionName' => $extKey,
         'workspaces' => 'live',
-        'path' => '/module/admin/' . $moduleIdentifier,
+        'path' => '/module/tools/' . $moduleIdentifier,
         'controllerActions' => [
             \CReifenscheid\DbRector\Controller\RectorController::class => 'index',
         ],

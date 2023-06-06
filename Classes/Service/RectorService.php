b88f0fc962a67b647a3cf79103fc8d62f350f668
creifenscheid <dev@christian-reifenscheid.de>
2023-06-06 09:00:11 +0200

    [TASK] Backend module | Init
    Adds method to create needed var folder

--- Classes/Service/RectorService.php
+++ Classes/Service/RectorService.php
@@ -35,6 +35,7 @@ use TYPO3\CMS\Core\Core\Environment;
  */
 class RectorService implements SingletonInterface
 {
+    private CONST EXT_KEY = 'db_rector';
     protected bool $goodToGo = false;
     protected ?string $version = null;
     protected ?string $path = null;
@@ -42,18 +43,49 @@ class RectorService implements SingletonInterface
     public function __construct()
     {
         $this->path = Environment::getProjectPath() . '/vendor/bin/rector ';
-        
+        $this->init();
+    }
+
+    private function init(): void
+    {
+        $goodToGo = true;
+
         $this->version = $this->run('--version');
-        
+
         /*
          * the shell returns either a string containing the version number or null
-         * null as return means, that an error occured
-         * and an error means, rector does not run properly or their is a problem with running rector
+         * null as return means, that an error occurred
+         * and an error means, rector does not run properly or there is a problem with running rector
          * so, we are not good to go.
          */
-        if (is_string($this->version)) {
-            $this->goodToGo = true;
+        if ($this->version === null) {
+            $goodToGo = false;
         }
+
+        $goodToGo = $this->initRectorConfiguration();
+
+        // assign state
+        $this->goodToGo = $goodToGo;
+    }
+
+    private function initRectorConfiguration(): bool
+    {
+        $folder = $this->createVarFolder();
+    }
+
+    private function createVarFolder(): bool
+    {
+        $varFolder = Environment::getVarPath() . '/' . self::EXT_KEY;
+
+        if (!file_exists($varFolder) && !mkdir($varFolder) && !is_dir($varFolder)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $varFolder));
+        }
+
+        if (!is_writable($varFolder)) {
+            return chmod($varFolder, $GLOBALS['TYPO3_CONF_VARS']['SYS']['folderCreateMask']);
+        }
+
+        return true;
     }
 
     public function getGoodToGo(): bool

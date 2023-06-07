07d03dfba8762a14ca609b5b66dd5eb4c92e4ce5
creifenscheid <dev@christian-reifenscheid.de>
2023-06-07 14:32:17 +0200

    [TASK] Backend module | TypoScript
    Implements index logic

--- Classes/Controller/TyposcriptController.php
+++ Classes/Controller/TyposcriptController.php
@@ -2,6 +2,12 @@
 
 namespace CReifenscheid\DbRector\Controller;
 
+use CReifenscheid\DbRector\Domain\Model\Element;
+use CReifenscheid\DbRector\Domain\Repository\ElementRepository;
+use Doctrine\DBAL\Exception;
+use TYPO3\CMS\Extbase\Persistence\Exception\IllegalObjectTypeException;
+use TYPO3\CMS\Extbase\Persistence\Generic\QueryResult;
+
 /***************************************************************
  *
  *  Copyright notice
@@ -32,13 +38,32 @@ namespace CReifenscheid\DbRector\Controller;
  */
 class TyposcriptController extends BaseController
 {
+    private CONST TABLE = 'sys_template';
+
+    protected ?ElementRepository $elementRepository = null;
+    protected array $propertiesToRefactor = [
+        'constants',
+        'config'
+    ];
+
+    public function injectElementRepository(ElementRepository $elementRepository): void
+    {
+        $this->elementRepository = $elementRepository;
+    }
+
     public function run(): void
     {
         //$filePath = Environment::getVarPath() . '/' . GeneralUtility::camelCaseToLowerCaseUnderscored($this->request->getControllerExtensionName());
         //$return = shell_exec(Environment::getProjectPath() . '/vendor/bin/rector process ' . $filePath . '/db_rector.typoscript' . ' --config ' . $filePath . '/rector.php');
 
-        // get all sys_template entries
-        // create rector models
+        $entries = $this->getDataEntries();
+
+        foreach ($entries as $entry) {
+            $this->createModel($entry);
+        }
+
+        $this->view->assign('elements', $this->elementRepository->findByOriginTable(self::TABLE));
+
         // show models in view
         // implement toolbar
             // run rector on entry
@@ -47,9 +72,47 @@ class TyposcriptController extends BaseController
 
     private function getDataEntries(): array
     {
-        $queryBuilder = $this->connectionPool->getQueryBuilderForTable('sys_template');
-        $queryBuilder->select('uid', 'constants', 'config')
+        $queryBuilder = $this->connectionPool->getQueryBuilderForTable(self::TABLE);
+        $queryBuilder->select('uid', 'pid', 'title', 'constants', 'config')
+        ->from(self::TABLE)
+        ->where(
+            $queryBuilder->expr()->eq('deleted', $queryBuilder->createNamedParameter(0)),
+        );
 
-        return[];
+        try {
+            return $queryBuilder->executeQuery()->fetchAllAssociative();
+        } catch (Exception $e) {
+        }
+
+        return [];
+    }
+
+    private function createModel(array $data): void
+    {
+        // CHECK IF MODEL ALREADY EXISTS
+        $existingModel = $this->elementRepository->findByUidAndTable($data['uid'], self::TABLE);
+        if (($existingModel instanceof QueryResult && $existingModel->count() > 0) || (is_array($existingModel) && count($existingModel) > 0)) {
+            return;
+        }
+
+        // PREPARE DATA TO REFACTOR
+        $dataToProcess = [];
+        foreach ($data as $key => $value) {
+            if (in_array($key, $this->propertiesToRefactor, true)) {
+                $dataToProcess[$key] = $value;
+            }
+        }
+
+        // SET UP MODEL
+        $element = new Element();
+        $element
+            ->setOriginUid($data['uid'])
+            ->setOriginTable(self::TABLE)
+            ->setOriginData($dataToProcess);
+
+        try {
+            $this->elementRepository->add($element);
+        } catch (IllegalObjectTypeException $e) {
+        }
     }
 }
